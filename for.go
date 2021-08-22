a2f2bf9bb70ef24214a7997e0c6484b8857f5823
Luciano Krebs <lkrebs05@gmail.com>
2021-08-22 11:50:32 -0300

    simple for loops example

---
+++ for.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func goForLoop() {
+	i := 1
+	for i <= 3 {
+		fmt.Printf("i: %v\n", i)
+		i = i + 1
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Printf("j: %v\n", j)
+	}
+
+	for {
+		fmt.Println("simple loop")
+		break
+	}
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Printf("n: %v\n", n)
+	}
+
+	fmt.Println("------------")
+}
--- main.go
+++ main.go
@@ -9,4 +9,5 @@ func main() {
 	values()
 	variables()
 	constants()
+	goForLoop()
 }
